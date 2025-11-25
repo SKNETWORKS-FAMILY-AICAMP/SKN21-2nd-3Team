dcb335da6382671c10db97ce3a7cbeda944c2087
Junreud <cngkdkr@gmail.com>
2025-11-25 10:37:44 +0900

    코드 수정

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -9,48 +9,48 @@ csv_file_path = sys.path[-1] + r"/data/raw/BankChurners.csv"
 
 
 def load_data(csv_file_path = csv_file_path):
-	"""CSV 파일을 불러와 데이터프레임 반환"""
-	df = pd.read_csv(csv_file_path)
-	return df
+    """CSV 파일을 불러와 데이터프레임 반환"""
+    df = pd.read_csv(csv_file_path)
+    return df
 
 def drop_column(df):
-	"""이탈 예측용 데이터 전처리"""
-	df = df.copy()
-	df['Attrition_Binary'] = (df['Attrition_Flag'] == 'Attrited Customer').astype(int)
-	drop_cols = [col for col in [
-		'CLIENTNUM',
-		'Attrition_Flag',
-		"Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2",
-		"Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1"
-	] if col in df.columns]
-	df = df.drop(columns=drop_cols)
-	return df
+    """이탈 예측용 데이터 전처리"""
+    df = df.copy()
+    df['Attrition_Binary'] = (df['Attrition_Flag'] == 'Attrited Customer').astype(int)
+    drop_cols = [col for col in [
+        'CLIENTNUM',
+        'Attrition_Flag',
+        "Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2",
+        "Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1"
+    ] if col in df.columns]
+    df = df.drop(columns=drop_cols)
+    return df
 
 def replace_nan_value(df, target_col='Attrition_Binary'):
-	"""Nan 값을 대체합니다. (범주형 칼럼: 최빈값, 숫자형 칼럼: 평균값)"""
-	categoriacal_cols = []
-	numerical_cols = []
-	df = df.copy()
+    """Nan 값을 대체합니다. (범주형 칼럼: 최빈값, 숫자형 칼럼: 평균값)"""
+    categoriacal_cols = []
+    numerical_cols = []
+    df = df.copy()
 
-	for col in df.select_dtypes(include='object').columns:
-		if col not in categoriacal_cols:
-			categoriacal_cols.append(col)
-		df[col] = df[col].fillna(df[col].mode()[0])
-	for col in df.select_dtypes(include=np.number).columns:
-		if col not in numerical_cols:
-			numerical_cols.append(col)
-		if col == target_col:
-			# 타깃 컬럼은 원래 분포를 유지해야 하므로 결측만 최빈값으로 처리
-			df[col] = df[col].fillna(df[col].mode()[0])
-			continue
-		
-		mean_val = df.loc[df[col] != 0, col].mean()  # 0이 아닌 값의 평균
-		df[col] = df[col].replace(0, np.nan)         # 0을 NaN으로 변경
-		df[col] = df[col].fillna(mean_val)
-	df = pd.get_dummies(df, drop_first=True)
-	# print(f"categoriacal_cols: {categoriacal_cols}")
-	# print(f"numerical_cols: {numerical_cols}")
-	return df
+    for col in df.select_dtypes(include='object').columns:
+        if col not in categoriacal_cols:
+            categoriacal_cols.append(col)
+        df[col] = df[col].fillna(df[col].mode()[0])
+    for col in df.select_dtypes(include=np.number).columns:
+        if col not in numerical_cols:
+            numerical_cols.append(col)
+        if col == target_col:
+            # 타깃 컬럼은 원래 분포를 유지해야 하므로 결측만 최빈값으로 처리
+            df[col] = df[col].fillna(df[col].mode()[0])
+            continue
+        
+        mean_val = df.loc[df[col] != 0, col].mean()  # 0이 아닌 값의 평균
+        df[col] = df[col].replace(0, np.nan)         # 0을 NaN으로 변경
+        df[col] = df[col].fillna(mean_val)
+    df = pd.get_dummies(df, drop_first=True)
+    # print(f"categoriacal_cols: {categoriacal_cols}")
+    # print(f"numerical_cols: {numerical_cols}")
+    return df
 
 def add_feature_engineering(df: pd.DataFrame) -> pd.DataFrame:
     """ML 전처리용 Feature Engineering 컬럼 생성"""
@@ -85,26 +85,26 @@ def add_feature_engineering(df: pd.DataFrame) -> pd.DataFrame:
 
 def select_features(df: pd.DataFrame) -> pd.DataFrame:
     """Feature Selection"""
-    df = df.copy()
-    
-
-def preprocess_pipeline(df):
-	# 전처리 파이프라인
-	# 1. 필요없는 칼럼 드랍
-	# 2. nan값 채우기
-	# 3. engineered feature 추가
-	
-    df = drop_column(df)
-    df = replace_nan_value(df)
-    df = add_feature_engineering(df)
 
     return df
 
-def feature_engineering(df):
-    """Feature Engineering"""
+def preprocess_pipeline(df):
+    # 전처리 파이프라인
+    # 1. 필요없는 칼럼 드랍
+    # 2. nan값 채우기
+
+    df = drop_column(df)
+    df = replace_nan_value(df)
+
+    return df
+
+def feature_engineering_pipeline(df):
+	# Feature Engineering 파이프라인
+
     df = df.copy()
     df = add_feature_engineering(df)
-	df = select_features(df)
+    df = select_features(df)
+
     return df
 
 import pandas as pd
@@ -113,7 +113,7 @@ import pandas as pd
 # df = preprocess_data(df)
 
 if __name__ == "__main__":
-	df =load_data()
-	df = preprocess_data(df)
-	# print(df['Attrition_Binary'].value_counts())
-	pass
+    df =load_data()
+    df = preprocess_pipeline(df)
+    # print(df['Attrition_Binary'].value_counts())
+    pass
