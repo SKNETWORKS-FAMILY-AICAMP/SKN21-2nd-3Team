fddc33a0af69af6dabc15733f915864fa2cb7d0d
Junreud <cngkdkr@gmail.com>
2025-11-26 15:04:29 +0900

    고객이탈예측 페이지 삭제

--- results/streamlit/main.py
+++ results/streamlit/main.py
@@ -24,11 +24,10 @@ st.set_page_config(
 
 st.set_page_config(page_title="SKN 2기 3팀 - 고객 관리 시스템")
 home    = st.Page("./dashboard.py", title="대시보드",   icon="🏠")
-test_sample  = st.Page("./predictor.py",  title="고객이탈 예측",   icon="🚗")
 message_center = st.Page("./message_center.py", title="고객 관리 메세지", icon="⚙️")
 data_chart = st.Page("./data_chart.py", title="고객이탈 확률 피드백", icon="📊")
 
-nav = st.navigation([home, test_sample, message_center, data_chart])
+nav = st.navigation([home, message_center, data_chart])
 nav.run()
 
 # def main():
