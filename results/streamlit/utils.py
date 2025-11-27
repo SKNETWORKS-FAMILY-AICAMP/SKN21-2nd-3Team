ac357db0e16872a5489e84e5db7a9635cca248a5
moon-613 <mjy6812@naver.com>
2025-11-27 09:07:09 +0900

    오류 해결

--- results/streamlit/utils.py
+++ results/streamlit/utils.py
@@ -1,13 +1,15 @@
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, '..', '..'))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
+
 from src.preprocessing import preprocess_pipeline, feature_engineering_pipeline
 import pandas as pd
 import streamlit as st
-import os
-import sys
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(current_dir, '../../'))
-if project_root not in sys.path:
-    sys.path.append(project_root)
 
 
 @st.cache_data
